64a7b5101eb3e1e495a0859ffd75b8eab90c4110
Milan Camus <31384089+MisterGoodDeal@users.noreply.github.com>
2021-07-10 17:52:19 +0200

    🚀  First commit

---
+++ chartjs_esgi.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package ChartJS_ESGI
+ * @version 1.0.0
+ */
+/*
+Plugin Name: ChartJS ESGI
+Plugin URI: https://turtlecorp.fr
+Description: This plugin allow an easy implementation of the ChartJS library inside Wordpress
+Author: Milan CAMUS & Arthur VADROT
+Version: 1.0.0
+Author URI: https://turtlecorp.fr
+*/
